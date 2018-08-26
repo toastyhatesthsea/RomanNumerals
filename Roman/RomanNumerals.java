7700cb9a64b5e7627159fd3c30f9d2930192b6a4
Saena <toastyhatesthesea@gmail.com>
2018-08-25 21:37:49 -0700

    Created Convert function

--- Roman/RomanNumerals.java
+++ Roman/RomanNumerals.java
@@ -1,16 +1,21 @@
 package Roman;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class RomanNumerals
 {
 
     public HashMap<String, Integer> numerals;
     public HashMap<String, Integer> beforeNumerals;
+    public HashSet<String> values;  //A hashset for making sure D, L, and V can each only appear once.
 
 
     public RomanNumerals()
     {
+        numerals = new HashMap<>();
+        values = new HashSet<>();
+
         numerals.put("V", 5);
         numerals.put("M", 1000);
         numerals.put("C", 100);
@@ -18,6 +23,35 @@ public class RomanNumerals
         numerals.put("X", 10);
         numerals.put("I", 1);
         numerals.put("D", 500);
+    }
+
+    public int convert(String numeral)
+    {
+        int answer = 0;
+        if (numeral.length() == 1)
+        {
+            return numerals.get(numeral);
+        } else if (numeral.length() > 0)
+        {
+            String firstNumeral;
+
+            for (int i = 0; i < numeral.length(); i++)
+            {
+                String secondNumeral = numeral.substring(i, i + 1);
+                if (i + 1 < numeral.length())
+                {
+                    firstNumeral = numeral.substring(i + 1, i + 2);
+                } else
+                {
+
+                }
+            }
+        }
+        else
+        {
+            return answer;
+        }
+
     }
 
     public boolean isSmaller(String numeralOne, String numeralTwo)
@@ -34,7 +68,16 @@ public class RomanNumerals
         }
     }
 
-
-
-
 }
+
+class RomanTesters{
+
+
+    public static void main(String[] agsgs)
+    {
+        RomanNumerals roman = new RomanNumerals();
+
+        boolean value = roman.isSmaller("X", "V");
+    }
+}
+
